8f111f37c7ee075afc84446f20195f776b0d84b0
Hubol <hubol.gordon@gmail.com>
2024-10-05 10:59:16 -0500

    Heart sounds

--- src/assets/generated/sounds/generated-sfx-data.ts
+++ src/assets/generated/sounds/generated-sfx-data.ts
@@ -1,10 +1,28 @@
 // This file is generated
 
 async function sfxs<T>(sfx: (ogg: string) => Promise<T>) {
-  const sounds = await Promise.all(["collect/valuable1.ogg"].map(sfx));
+  const sounds = await Promise.all(
+    [
+      "collect/valuable1.ogg",
+      "fx/heart1.ogg",
+      "fx/heart2.ogg",
+      "fx/heart3.ogg",
+      "fx/heart4.ogg",
+      "fx/heart5.ogg",
+      "fx/heart6.ogg",
+    ].map(sfx),
+  );
   return {
     Collect: {
       Valuable1: sounds[0],
+    },
+    Fx: {
+      Heart1: sounds[1],
+      Heart2: sounds[2],
+      Heart3: sounds[3],
+      Heart4: sounds[4],
+      Heart5: sounds[5],
+      Heart6: sounds[6],
     },
   };
 }
--- src/igua/objects/fx/obj-fx-heart.ts
+++ src/igua/objects/fx/obj-fx-heart.ts
@@ -3,9 +3,19 @@ import { Tx } from "../../../assets/textures";
 import { lerp } from "../../../lib/game-engine/routines/lerp";
 import { Rng } from "../../../lib/math/rng";
 import { sleep } from "../../../lib/game-engine/routines/sleep";
+import { Sfx } from "../../../assets/sounds";
 
 const tints = [0xEC524A, 0xECAADD];
 let tintIndex = 0;
+
+const sfxHearts = [
+    // Sfx.Fx.Heart1,
+    // Sfx.Fx.Heart2,
+    // Sfx.Fx.Heart3,
+    // Sfx.Fx.Heart4,
+    Sfx.Fx.Heart5,
+    // Sfx.Fx.Heart6,
+];
 
 export function objFxHeart() {
     const yd = Rng.float(-0.3, -1.4);
@@ -18,7 +28,11 @@ export function objFxHeart() {
             self.y += yd;
         })
         .coro(function* (self) {
-            yield lerp(self.scale, "x").to(Rng.choose(1, -1)).over(Rng.float(100, 500));
+            const target = Rng.choose(1, -1);
+            const time = Rng.float(100, 500);
+            yield lerp(self.scale, "x").to(target / 2).over(time / 2);
+            Rng.choose(...sfxHearts).with.rate(Rng.float(0.7, 1)).play();
+            yield lerp(self.scale, "x").to(target).over(time / 2);
             yield sleep(Rng.float(400, 800));
             yield lerp(self.scale, "x").to(0).over(Rng.float(100, 500));
             self.destroy();
