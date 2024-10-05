0297e657112b915d510e5bad4744d3f056827233
Hubol <hubol.gordon@gmail.com>
2024-10-05 15:05:39 -0500

    Decoration

--- src/igua/scenes/scn-level1.ts
+++ src/igua/scenes/scn-level1.ts
@@ -1,5 +1,32 @@
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
+import { Coro } from "../../lib/game-engine/routines/coro";
+import { lerp } from "../../lib/game-engine/routines/lerp";
+import { blendPixiColor } from "../../lib/pixi/blend-pixi-color";
+import { container } from "../../lib/pixi/container";
+import { Cutscene } from "../globals";
+import { playerObj } from "../objects/obj-player";
 
 export function scnLevel1() {
     Lvl.Level1();
+
+    Cutscene.play(function* () {
+        playerObj.scale.x = 0;
+        playerObj.alpha = 0;
+        playerObj.step(() => {
+            playerObj.scale.y = Math.sign(playerObj.scale.y) * playerObj.scale.x;
+        });
+
+        // const blender = container().merge({ factor: 0 }).step(self => {
+
+        //     playerObj.tint = blendPixiColor(windowInitialTint, 0xADDAFF, Math.round(self.factor * 5) / 5);
+        // }).show();
+
+        // playerObj.speed.x = 1;
+        yield* Coro.all([
+            // lerp(blender, "factor").to(1).over(1000),
+            lerp(playerObj, "alpha").to(1).over(1000),
+            lerp(playerObj.scale, "x").to(1).over(1000),
+        ]);
+        // playerObj.speed.x = 0;
+    });
 }
