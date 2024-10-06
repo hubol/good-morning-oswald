01dc7fac02716b3c074d7895cae66f4f39f06d32
Hubol <hubol.gordon@gmail.com>
2024-10-06 16:24:28 -0500

    Money sounds

--- src/assets/generated/sounds/generated-sfx-data.ts
+++ src/assets/generated/sounds/generated-sfx-data.ts
@@ -3,6 +3,7 @@
 async function sfxs<T>(sfx: (ogg: string) => Promise<T>) {
   const sounds = await Promise.all(
     [
+      "collect/money.ogg",
       "collect/valuable1.ogg",
       "dialog/close.ogg",
       "dialog/open.ogg",
@@ -12,29 +13,32 @@ async function sfxs<T>(sfx: (ogg: string) => Promise<T>) {
       "fx/heart4.ogg",
       "fx/heart5.ogg",
       "fx/heart6.ogg",
+      "fx/money count.ogg",
       "player/duck.ogg",
       "player/unduck.ogg",
     ].map(sfx),
   );
   return {
     Collect: {
-      Valuable1: sounds[0],
+      Money: sounds[0],
+      Valuable1: sounds[1],
     },
     Dialog: {
-      Close: sounds[1],
-      Open: sounds[2],
+      Close: sounds[2],
+      Open: sounds[3],
     },
     Fx: {
-      Heart1: sounds[3],
-      Heart2: sounds[4],
-      Heart3: sounds[5],
-      Heart4: sounds[6],
-      Heart5: sounds[7],
-      Heart6: sounds[8],
+      Heart1: sounds[4],
+      Heart2: sounds[5],
+      Heart3: sounds[6],
+      Heart4: sounds[7],
+      Heart5: sounds[8],
+      Heart6: sounds[9],
+      MoneyCount: sounds[10],
     },
     Player: {
-      Duck: sounds[9],
-      Unduck: sounds[10],
+      Duck: sounds[11],
+      Unduck: sounds[12],
     },
   };
 }
--- src/igua/objects/obj-overlay.ts
+++ src/igua/objects/obj-overlay.ts
@@ -6,6 +6,8 @@ import { Collections } from "../systems/collections";
 import { Instances } from "../../lib/game-engine/instances";
 import { objNpcMessage } from "./obj-npc";
 import { approachLinear } from "../../lib/math/number";
+import { CollisionShape } from "../../lib/pixi/collision";
+import { Sfx } from "../../assets/sounds";
 
 function computeMoney() {
     return (Collections.severanceMoney ? 900 : 0) + Collections.looseMoneyUids.size;
@@ -14,18 +16,46 @@ function computeMoney() {
 function objMoneyCounter() {
     const tint = 0xC1323E;
     let alpha = 1;
+    let previousValue: number | null = null;
+    let currentValue = 0;
+    let shownComputedValueSteps = 0;
 
+    const differentialObj = objText.LargeBold("", { tint }).at(0, 35);
+    const totalObj = objText.LargeScribbleDigits("0", { tint });
     return container(
         new Graphics().lineStyle({ color: tint, width: 1 }).moveTo(0, 4).lineTo(32, 4).moveTo(80 + 15, 4).lineTo(
             112 + 15,
             4,
         ),
         objText.Large("Your Funds", { tint }).at(36, 0),
-        objText.LargeScribbleDigits("0", { tint }).step(self => {
-            self.text = computeMoney().toString();
-        }).at(0, 10),
-    ).at(4, 4)
+        totalObj.at(0, 10),
+        differentialObj,
+    )
+        .collisionShape(CollisionShape.Children)
+        .at(4, 4)
         .step(self => {
+            const nextValue = computeMoney();
+            if (currentValue !== nextValue) {
+                if (previousValue === null) {
+                    previousValue = currentValue;
+                }
+                shownComputedValueSteps = 0;
+            }
+            else {
+                shownComputedValueSteps++;
+                if (shownComputedValueSteps >= 90) {
+                    previousValue = null;
+                }
+            }
+
+            currentValue = approachLinear(currentValue, nextValue, 21);
+            totalObj.text = currentValue.toString();
+            const previousDifferentialObjText = differentialObj.text;
+            differentialObj.text = previousValue === null ? "" : `+${nextValue - previousValue}`;
+            if (differentialObj.text && previousDifferentialObjText !== differentialObj.text) {
+                Sfx.Collect.Money.play();
+            }
+
             alpha = approachLinear(
                 alpha,
                 !Instances(objNpcMessage).some(x => x.visible && x.collides(self)) ? 1 : 0,
